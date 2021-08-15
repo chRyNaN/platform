4fb5c5935eee9b96cf95d003d89769ff7fc9a2b5
Chris <ckeenan@starry.com>
2021-08-15 12:52:15 -0500

    Bumped version to 0.2.0 (2)

--- buildSrc/src/main/java/com.chrynan.platform.buildSrc/LibraryConstants.kt
+++ buildSrc/src/main/java/com.chrynan.platform.buildSrc/LibraryConstants.kt
@@ -6,9 +6,9 @@ object LibraryConstants {
     const val group = "com.chrynan.platform"
     const val owner = "chrynan"
     const val repoName = "platform"
-    const val versionName = "0.1.5"
-    const val versionCode = 1
-    const val versionDescription = "Release 0.1.5"
+    const val versionName = "0.2.0"
+    const val versionCode = 2
+    const val versionDescription = "Release 0.2.0"
     const val license = "Apache-2.0"
     const val vcsUrl = "https://github.com/chRyNaN/platform.git"
 
